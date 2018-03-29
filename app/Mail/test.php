5d15e2824f9b1939dca8d3f96e3eaaf5931b55f1
mafisz <mafisz@gmail.com>
2018-03-29 11:26:50 +0200

    test mail class name

--- app/Mail/test.php
+++ app/Mail/test.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class test extends Mailable
+class Test extends Mailable
 {
     use Queueable, SerializesModels;
 
