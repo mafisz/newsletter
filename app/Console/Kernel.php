6237a9ad1dae6a1fa2b0204feeb6c96e9113df7f
mafisz <mafisz@gmail.com>
2018-03-29 11:22:39 +0200

    cron run queue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
          Commands\SendNewsletter::class,
+         Commands\SendEmails::class,
     ];
 
     /**
@@ -25,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('newsletter:send')->everyMinute();
+        $schedule->command('email:send')->everyMinute();
     }
 
     /**
